1a14f3ff1e69bfa45619d8e5099f22be0abbdd43
Thanyah Kung <pitchayarasm.kunghae@hotmail.com>
2019-04-15 00:05:15 -0700

    Updates

---
+++ precache-manifest.5abbed7317500a0359a329311e48b201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d94c1de13e205df9482a",
+    "url": "/static/css/main.337d6709.chunk.css"
+  },
+  {
+    "revision": "d94c1de13e205df9482a",
+    "url": "/static/js/main.cf319f72.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e98ca6f90cf5272a2051",
+    "url": "/static/js/2.cb1d723a.chunk.js"
+  },
+  {
+    "revision": "fd6ccd1e9534038575fd9b9d37585fb9",
+    "url": "/index.html"
+  }
+];
